654e30ef96ee6d8b395df7cabfeb76e37c3b79cc
Pallavi-Chundru <chpallavi80@gmail.com>
2024-08-14 10:27:22 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React examples</h1>
+        <h1>Hello Pallavi</h1>
       </header>
     </div>
   );
